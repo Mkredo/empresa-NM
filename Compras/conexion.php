76dda92238743bbeb2be84e49ee984daea7aeacb
alonsin <halonso.lopez90@gmail.com>
2025-03-20 10:38:22 -0600

    cambio base de datos php

--- Compras/conexion.php
+++ Compras/conexion.php
@@ -1,26 +1,23 @@
 <?php
 
-echo"Hola soy el archivo php";
+echo "Hola soy el archivo php";
 
 $servidor = "localhost";
 $usuario = "root";
 $password = "";
 $base_datos = "empresanm";
 
-$conexion = new mysqli($servidor,$usuario,$password,$base_datos);
+$conexion = new mysqli($servidor, $usuario, $password, $base_datos);
 if ($conexion) {
     echo "estoy conectado";
-}
-else {
+} else {
     echo "no estoy conectado";
 }
 
 
-$consulta = "select * from usuarios";
+$consulta = "select * from usuario";
 $result = $conexion->query($consulta);
 
-while ($fila = $result->fetch_assoc()){
-    echo " ID: " . $fila["id"] . "Nombre". $fila["nombre_usuario"] ."Nickname". $fila["nickname"] . "<br>";
+while ($fila = $result->fetch_assoc()) {
+    echo " ID: " . $fila["id"] . "Nombre" . $fila["nombre_usuario"] . "Nickname" . $fila["nickname"] . "<br>";
 }
-
-echo $result; 
