0db19c599557ef0f62b1e0eee6ac57823e8d8ac6
alonsin <halonso.lopez90@gmail.com>
2025-03-25 10:16:54 -0600

    cambios nuevo cliente

--- Clientes/index.php
+++ Clientes/index.php
@@ -4,7 +4,8 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   <title>Modulo de Compras</title>
 </head>
 
@@ -18,11 +19,13 @@
 
   <div class="container text-center p-4">
 
-    <table class="table p-4">
+    <button data-bs-toggle="modal" data-bs-target="#modalClientes" type="button" class="btn btn-success">NUEVO CLIENTE</button>
+
+    <table class="table p-4 mt-4">
       <thead class="thead-dark">
         <tr>
           <th scope="col">#</th>
-          <th scope="col">Nombre Usuario</th>
+          <th scope="col">Nombre Cliente</th>
           <th scope="col">NickName</th>
           <th scope="col">Accciones</th>
         </tr>
@@ -47,7 +50,32 @@
 
       </tbody>
     </table>
+
+    <div class="modal fade" id="modalClientes" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Ingresa Datos del Cliente</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <label for="">Nombre de Cliente</label>
+            <input type="text" class="form-control">
+            <label for="">Usuario Cliente</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+            <button type="button" class="btn btn-primary">Guardar Datos</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
+
+
+
 
 
 
