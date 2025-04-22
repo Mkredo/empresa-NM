0eb8a9ca5ee404886afa4adbe9ae8969927043ea
unknown <damerica1402@gmail.com>
2025-04-21 19:50:38 -0600

    boton eliminar

---
+++ Compras/eliminar.php
@@ -0,0 +1,23 @@
+<?php
+
+include 'conexion.php';
+
+// Verifica si se recibió el ID por GET
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    // Eliminar el registro
+    $sql = "DELETE FROM compras WHERE id = $id";
+    if ($conexion->query($sql) === TRUE) {
+        // Redireccionar al listado principal
+        header("Location: index.php");
+        exit();
+    } else {
+        echo "Error al eliminar: " . $conexion->error;
+    }
+} else {
+    echo "ID no recibido";
+}
+
+$conexion->close();
+?>
